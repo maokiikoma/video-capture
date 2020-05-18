8dfa83a4b84060c6503a08f533c380b9398a17d2
maoki maoki <maoki.ikoam@gmail.com>
2020-05-18 23:54:34 +0900

    first commit

---
+++ VideoCapture/VideoCapture/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Lib;
+
+namespace VideoCapture
+{
+    class Program
+    {
+        const int DefaultInterval = 200; // フレームを取得する間隔（ミリ秒）
+        const string DefaultOutPath = @"C:\Users\user\Pictures\capture";
+
+        static void Main(string[] args)
+        {
+            string fullFileName = null;
+            string outPath = null;
+            int interval = -1;
+            try
+            {
+                switch (args.Length)
+                {
+                    case 3:
+                        fullFileName = args[0];
+                        outPath = args[1];
+                        interval = int.Parse(args[2]);
+                        break;
+                    case 2:
+                        fullFileName = args[0];
+                        outPath = args[1];
+                        break;
+                    case 1:
+                        fullFileName = args[0];
+                        break;
+                    default:
+                        PrintHelpMessage();
+                        return;
+                }
+
+                var lib = new VideoLib();
+                lib.Exec(fullFileName, outPath, interval);
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        private static void PrintHelpMessage()
+        {
+            Console.WriteLine("arg0: fileFullName.");
+            Console.WriteLine($"arg1: outPath.  default value: {DefaultOutPath}");
+            Console.WriteLine($"arg2: interval(milliseconds).  default value: {DefaultInterval}");
+        }
+    }
+}
---
+++ VideoCapture/VideoCaptureApp/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Lib;
+using Microsoft.Win32;
+
+namespace VideoCaptureApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Title = "ファイルを開く";
+            dialog.Filter = "全てのファイル(*.*)|*.*";
+            if (dialog.ShowDialog() == true)
+            {
+                this.textBoxFileName.Text = dialog.FileName;
+            }
+            else
+            {
+                this.textBoxFileName.Text = "キャンセルされました";
+            }
+        }
+
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var lib = new VideoLib();
+                var result = lib.Exec(this.textBoxFileName.Text, this.textBoxOutPath.Text, int.Parse(this.textBoxInterval.Text));
+                if (result)
+                {
+                    MessageBox.Show("正常終了しました。", "message box", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                {
+                    MessageBox.Show("入力がおかしいです。", "message box", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                MessageBox.Show("エラーが発生しました。", "message box", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+    }
+}
